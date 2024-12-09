caf9341c0a3f2591a0b8e45c0a5284e0b5a8d96f
MaxDamageDevStuff <68134538+MaxDamageDevStuff@users.noreply.github.com>
2024-12-09 19:39:07 +0100

    aggiunte tabelle articoli e autori
    sebbene le tabelle aggiunte fossero usate dall'intero codice, queste non erano state inserite nel file di creazione delle tabelle

--- tabelle.sql
+++ tabelle.sql
@@ -1,3 +1,17 @@
+CREATE TABLE articoli (
+    titolo VARCHAR(255) NOT NULL,
+    autore_articolo INT,
+    data_creazione TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE autori (
+    nickname VARCHAR(255) NOT NULL,
+    password VARCHAR(512) NOT NULL,
+    rating DOUBLE PRECISION,
+    email VARCHAR(255) NOT NULL,
+    id_autore INT NOT NULL
+);
+
 CREATE TABLE testi_frasi (
 	id_testo_frase INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	testo VARCHAR (255),
@@ -10,11 +24,11 @@ CREATE TABLE testi_frasi (
 
 CREATE TABLE contesti_frasi (
 	id_contesto INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	data_creazione DATE NOT NULL,
+	data_creazione TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 	posizione INT NOT NULL,
 	accettazione BOOL DEFAULT false NOT NULL,
-	data_accettazione DATE,
-	data_aggiornamento DATE,
+	data_accettazione TIMESTAMP WITHOUT TIME ZONE,
+	data_aggiornamento TIMESTAMP WITHOUT TIME ZONE,
 	autore_contesto INT NOT NULL,
 	testo_frase INT NOT NULL,
 	collegamento VARCHAR(255) DEFAULT NULL,
